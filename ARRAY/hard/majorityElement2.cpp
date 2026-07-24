57c2752eb9027180c46591181ce38e1a46f2c96c
iamsidarthpathak <er.sidarthpathak@gmail.com>
2026-07-24 10:58:45 +0530

    updated majority 2 by better soln usin hashing

--- ARRAY/hard/majorityElement2.cpp
+++ ARRAY/hard/majorityElement2.cpp
@@ -4,19 +4,37 @@ using namespace std;
 vector<int>majorityelement(vector<int>&arr){
     int n = arr.size();
     //brute
-    vector<int>ans;
-    int m = n/3;
-    for (int i=0;i<n ;i++){
-        int cnt =0;
-        for(int j=0;j<n;j++){
-            if(arr[j]==arr[i]){
-                cnt++;
-            }
+    // vector<int>ans;
+    // int m = n/3;
+    // for (int i=0;i<n ;i++){
+    //     int cnt =0;
+    //     if(ans.size()==0||ans[0]!=arr[i]){
+    //         for(int j=0;j<n;j++){
+    //         if(arr[j]==arr[i]){
+    //             cnt++;
+    //         }
             
-            }if(cnt>m){
-                ans.push_back(arr[i]);
+    //         }if(cnt>m){
+    //             ans.push_back(arr[i]);
+    //     }
+    //     if(ans.size()==2)break;
+    // }
+    //     }
+        //better approach by hashing
+
+        vector<int>ans;
+        int m =n/3;
+        unordered_map<int, int>mpp;
+        for(int i=0;i<n ;i++){
+            mpp[arr[i]]++;
         }
-    }return ans;
+        for(auto x :mpp){
+            if(x.second>m){
+                ans.push_back(x.first);
+                if(ans.size()==2)break;
+            }
+        }
+    return ans;
 }
 
 int main (){
