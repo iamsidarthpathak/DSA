2bbe2a408768d198bdf85b52c8ebc03ce6a26460
iamsidarthpathak <er.sidarthpathak@gmail.com>
2026-07-26 00:03:49 +0530

    solve three sum by optimal approach using 3 pointer

--- ARRAY/hard/threeSum.cpp
+++ ARRAY/hard/threeSum.cpp
@@ -22,20 +22,48 @@ vector<vector<int>>findlist(vector<int>&nums,int target){
     // }
     //   vector<vector<int>>ans(st.begin(),st.end());
             //better approach
-        set<vector<int>>res;
-        for(int i=0;i<n;i++){
-            unordered_set<int>st;//t(c)=O(n2log(m))
-            for(int j=i+1;j<n;j++){//s(C)=O(n+m+m)
-                int third=target-(nums[i]+nums[j]);
-                if(st.find(third)!=st.end()){
-                    vector<int>temp={nums[i],nums[j],third};
-                    sort(temp.begin(),temp.end());
-                    res.insert(temp);
-                }
-                st.insert(nums[j]);
+
+
+        // set<vector<int>>res;
+        // for(int i=0;i<n;i++){
+        //     unordered_set<int>st;//t(c)=O(n2log(m))
+        //     for(int j=i+1;j<n;j++){//s(C)=O(n+m+m)
+        //         int third=target-(nums[i]+nums[j]);
+        //         if(st.find(third)!=st.end()){
+        //             vector<int>temp={nums[i],nums[j],third};
+        //             sort(temp.begin(),temp.end());
+        //             res.insert(temp);
+        //         }
+        //         st.insert(nums[j]);
+        //     }
+        // }
+        //     vector<vector<int>>ans(res.begin(),res.end());
+
+            //optimal approach
+        //we will be using two pinter approach or may be 3 pointer
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+    for(int i=0;i<n;i++){
+        if(i>0&&nums[i-1]==nums[i])continue;
+        int j =i+1;
+        int k=n-1;
+        while (j<k){
+            if((nums[i]+nums[j]+nums[k])<target){
+                j++;
             }
+            else if((nums[i]+nums[j]+nums[k])>target){
+                k--;
+            }
+            else{
+               ans.push_back({nums[i],nums[j],nums[k]});
+                j++;
+                k--;
+                while(j<k&&nums[j]==nums[j-1])j++;
+                while(j<k&&nums[k]==nums[k+1])k--;
+            }
+
         }
-            vector<vector<int>>ans(res.begin(),res.end());
+    }
 
     return ans;
 }
