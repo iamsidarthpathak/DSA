5283b84f9052bfc29ccde586950ee5187042ede0
iamsidarthpathak <er.sidarthpathak@gmail.com>
2026-07-26 10:10:56 +0530

    solve four sum with hashing approach

--- ARRAY/hard/fourSum.cpp
+++ ARRAY/hard/fourSum.cpp
@@ -4,24 +4,43 @@ using namespace std;
 vector<vector<int>>findQuad(vector<int>&nums,int target){
     int n =nums.size();
     //brute force
-    set<vector<int>>st;
-    for(int i=0;i<n;i++){
+    // set<vector<int>>st;
+    // for(int i=0;i<n;i++){
+    //     for(int j=i+1;j<n;j++){//t(C)O(n4)
+    //         for(int k =j+1;k<n;k++){
+    //             for(int l =k+1;l<n;l++){
+    //             long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+
+    //                 if(sum==target){
+    //                     vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+    //                     sort(temp.begin(),temp.end());
+    //                     st.insert(temp);//s(c)=2*log(m)
+
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+        //better approach
+        set<vector<int>>result;
+        for(int i=0;i<n;i++){
+           
         for(int j=i+1;j<n;j++){
-            for(int k =j+1;k<n;k++){
-                for(int l =k+1;l<n;l++){
-                long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
-
-                    if(sum==target){
-                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-
-                    }
+             unordered_set<int>st;
+             for(int k =j+1;k<n;k++){
+                 long long sum = (long long)nums[i] + nums[j] + nums[k] ;
+                long long fourth = (long long )target-sum;
+                if(st.find(fourth)!=st.end()){
+                    vector<int>temp={nums[i],nums[j],nums[k],(int)fourth};
+                    sort(temp.begin(),temp.end());
+                    result.insert(temp);
                 }
+                st.insert(nums[k]);
+             }
             }
         }
-    }
-   vector<vector<int>>ans(st.begin(),st.end());
+   vector<vector<int>>ans(result.begin(),result.end());
    return ans;
 }
 int main(){
@@ -40,5 +59,6 @@ int main(){
         }cout<<endl;
     }
 
-    return 0;
+    return 0;//nums = [1,0,-1,0,-2,2]
+//target = 0
 }
