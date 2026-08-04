bf08b651ab5e1add2d6e41d73919e5d82ae21bf3
iamsidarthpathak <er.sidarthpathak@gmail.com>
2026-08-04 14:30:36 +0530

    learn the concept of lower bound

---
+++ BINARY_SEATRCH/1D_ARR/lowerBound.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lowerbound(vector<int>&nums,int target){
+    int low=0;
+    int high=nums.size()-1;
+    int ans = nums.size();
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        if(nums[mid]>=target){
+            ans = mid;
+            high = mid -1;
+
+        }
+        else low = mid+1;
+        
+
+    }
+    return ans;
+}
+int main(){
+    int n ;
+    cin>>n;
+    vector<int>nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+
+    }
+    int target;
+    cin>>target;
+
+    int res=lowerbound(nums,target);
+    cout<<res;
+
+}
